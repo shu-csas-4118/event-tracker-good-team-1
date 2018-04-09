4ceafb9085ffb46a9832db10e0d21a1f190ef7a5
Marc Good <marc.good@student.shu.edu>
2018-04-09 09:21:26 -0400

    Login Page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+var login = require('./routes/login');
 var users = require('./routes/users');
 
 var app = express();
@@ -23,7 +24,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/index', index);
 app.use('/users', users);
+app.use('/login', login);
 
 app.listen(8080);
 
--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index1', { title: 'Express2'});
+  res.render('login', { title: 'Express2'});
   
 });
 
