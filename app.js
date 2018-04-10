1af592bd1ee41e8710bb2ed05ec954355df51ff2
Adriano Soares <adriano.soares@student.shu.edu>
2018-04-09 20:23:27 -0400

    Organize folders

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var login = require('./routes/login');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -22,11 +17,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/index', index);
-app.use('/users', users);
-app.use('/login', login);
 
 app.listen(8080);
 
