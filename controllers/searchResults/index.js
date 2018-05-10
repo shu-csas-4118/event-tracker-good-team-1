ce04a434d3bd0e6498629d6d9cbcec7acf05a8c4
Adriano Soares <adriano.soares@student.shu.edu>
2018-05-10 15:44:46 -0400

    add bootstrap bar

--- controllers/searchResults/index.js
+++ controllers/searchResults/index.js
@@ -1,18 +1,17 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 const Event = require('../../models/event');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const stuff = ["This is an item", "this is another item"];
   Event.find({}).maxTime(10000).exec(function(err, events){
     if(err){
       console.error(err);
       res.status(500).redirect('/error');
     }
-    else res.status(200).render('searchViews/search', {events: events});
+    else res.status(200).render(path.join(__dirname, 'views/search'), {events: events});
   });
-
 });
 
 module.exports = router;
