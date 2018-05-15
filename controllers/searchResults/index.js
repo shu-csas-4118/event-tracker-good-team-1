fc5da4355f4824faf9c9bd8dbb81a13e4af6a9f0
Adriano Soares <adriano.soares@student.shu.edu>
2018-05-15 18:00:09 -0400

    add search feature

--- controllers/searchResults/index.js
+++ controllers/searchResults/index.js
@@ -14,4 +14,24 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/:event', function(req, res, next){
+    const event = req.params.event;
+    Event.find({name: { $regex: new RegExp(event, 'i') } }).maxTime(10000).exec(function(err, events){
+        if(err){
+            console.error(err);
+            res.status(500).redirect('/error');
+        }
+        else res.status(200).render(path.join(__dirname, 'views/search'), {events: events});
+    });
+});
+
+router.post('/', function(req, res, next) {
+    if(req.body.event)
+        res.status(302).redirect(`events/${req.body.event}`);
+    else{
+        console.error(`The search bar couldn't find any results. The post body was undefined.`);
+        res.status(404).redirect('/error');
+    }
+});
+
 module.exports = router;
