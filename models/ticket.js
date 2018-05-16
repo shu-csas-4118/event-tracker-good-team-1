de19c5924b4dd0e4c6af3c06473c1af04d468441
marcgood1996 <goodmarc@shu.edu>
2018-05-15 20:15:46 -0400

    Added the ticket model

---
+++ models/ticket.js
@@ -0,0 +1,38 @@
+const hash = require('./utils/hash').hash;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TicketSchema = new Schema({
+   firstName: {
+       type: String,
+       max: 15,
+       required: true
+   },
+   lastName:{
+       type: String,
+       max: 15,
+       required: true
+   },
+   email:{
+    type: String,
+    
+    required: true
+    },
+   eventId:{
+       type: String,
+       required: false
+   },
+   phoneNumber:{
+      type: String,
+      required: false
+   },
+   username:{
+    type: String,
+    required: false
+ }
+});
+
+
+const Ticket = mongoose.model('Ticket', TicketSchema);
+
+module.exports = Ticket;
