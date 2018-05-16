0379447c38b61337ecd9d101429d0dc4ed4e0940
marcgood1996 <goodmarc@shu.edu>
2018-05-15 21:19:00 -0400

    Registration and ticket schema

--- controllers/eventPage/index.js
+++ controllers/eventPage/index.js
@@ -11,9 +11,17 @@ router.get('/', function(req, res){
     else res.status(200).json(events);
   });
 });
+router.get('/:user_id', function(req, res){
+    const id = req.params.user_id;
+    Event.findById(id, function(err, event){
+      if(err)
+        res.send(err);
+      else res.status(200).render(path.join(__dirname, 'views/eventPage'), {event: event});
+    });
+  });
 router.post('/register', function(req, res, next){
     let body = req.body;
-
+   
     Ticket.findOne({firstName: body.firstName}, function(error, ticket){
         if(error){
             console.error(error.message);
@@ -21,6 +29,7 @@ router.post('/register', function(req, res, next){
             let tic = new Ticket();
             tic.firstName = body.firstName;
             tic.lastName = body.lastName;
+            tic.eventId = body.idnum;
             //acct.address = body.address;
             //acct.events.push(mongoose.Types.ObjectId('5af3de6bbd6a793388bdf1fe'));
 
@@ -33,13 +42,6 @@ router.post('/register', function(req, res, next){
 
     next();
 });
-router.get('/:user_id', function(req, res){
-  const id = req.params.user_id;
-  Event.findById(id, function(err, event){
-    if(err)
-      res.send(err);
-    else res.status(200).render(path.join(__dirname, 'views/eventPage'), {event: event});
-  });
-});
+
 
 module.exports = router;
